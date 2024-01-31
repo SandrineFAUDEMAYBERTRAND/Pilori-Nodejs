7dae085e4226e5bbd7530d8f26aea44c8fbf3d24
SandrineFAUDEMAYBERTRAND <sandrine.faudemay-bertrand@oclock.school>
2024-01-31 15:23:03 +0100

    4. Routes

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-console.log("bonjour")
+//console.log("bonjour")
+import express from "express";
+import * as dotenv from 'dotenv';
+import session from "express-session";
+dotenv.config();
 
-/* import * as dotenv from 'dotenv';
-dotenv.config(); */
+const port = process.env.PORT || 3000;
+const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
 
+app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: process.env.SESSION_SECRET
+}));
 
+app.use(express.static('./S06-inte-pilori-sandrineFAUDEMAYBERTRAND/public'));
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
